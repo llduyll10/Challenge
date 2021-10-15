d4740e3faf15b6bda786e97e01516878f45f0fdf
duynnd <>
2021-10-15 19:04:23 +0700

    init app

---
+++ src/views/Layout/Layout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Header />
+    <div class="drawer">
+      <div class="left-drawer">
+        <Drawer />
+      </div>
+      <div class="right-drawer">
+        <router-view />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Drawer from "../../components/Drawer";
+import Header from "../../components/Header";
+export default {
+  name: "Layout",
+  components: { Drawer, Header },
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ src/views/TestAPI.vue
@@ -0,0 +1,34 @@
+<template>
+    <button class="btn btn-danger" @click="callAPI">
+        Call api
+    </button>
+</template>
+<script>
+import axios from 'axios'
+export default {
+    methods:{
+        callAPI(){
+            var obj = {
+                "email":"duy10@gmail.com",
+                "password":"123456"
+            }
+            let axiosConfig = {
+                headers: {
+                        'Content-type': 'application/json',
+                        'Access-Control-Allow-Origin':'*',
+                        'Access-Control-Allow-Methods':'GET,POST,PUT,DELETE,PATCH',
+                        'Access-Control-Allow-Headers':'Origin, Content-Type, X-Auth-Token'
+                    }
+            };
+
+            axios.post('https://peaceful-journey-28732.herokuapp.com/api/login',obj,axiosConfig)
+                .then(res =>{
+                    console.log('res',res)
+                })
+                .catch(err =>{
+                    console.log('err',err)
+                })
+        }
+    }
+}
+</script>
